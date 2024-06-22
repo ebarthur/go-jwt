1fb4a7d8c0974c72ba49acf38ac17a37395c868a
lasagna <arthurebenezer@aol.com>
2024-06-22 09:35:02 +0000

    feat: write tests

---
+++ test/userscontroller_test.go
@@ -0,0 +1,17 @@
+package test
+
+import "testing"
+
+// TODO: Test sign-up, login and reset password
+
+func TestSignUp(t *testing.T) {
+
+}
+
+func TestLogin(t *testing.T) {
+
+}
+
+func TestPasswordReset(t *testing.T) {
+
+}
